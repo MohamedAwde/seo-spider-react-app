34d60defc6a376fe38f700b51b38b054f6912b80
Mohamed Awde <116069156+MohamedAwde@users.noreply.github.com>
2022-11-25 11:37:54 +0200

    making the loading indicator full screen

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 const LoadingSpinner = () => {
+  const loadingResults = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    loadingResults.current?.scrollIntoView({ behavior: "smooth" });
+  }, []);
+
   return (
-    <div className=" my-4">
-      <div className=" flex justify-center items-center my-2">
-        <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-gray-900"></div>
+    <div
+      className="flex items-center justify-center h-screen"
+      ref={loadingResults}
+    >
+      <div className=" my-4">
+        <div className=" flex justify-center items-center my-2">
+          <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-gray-900"></div>
+        </div>
+        <h4 className="text-center">Analyzing your website</h4>
       </div>
-      <h4 className="text-center">Analyzing your website</h4>
     </div>
   );
 };
